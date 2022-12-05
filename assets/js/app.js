9959ac45ad5b5385588bce37155e87e529529c79
Nilabh Shivam Thakur <96217047+shvm-k@users.noreply.github.com>
2022-12-05 11:52:40 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () => {
+    if(this.scrollY > 0) {
+        navbar.classList.remove('py-4')
+        navbar.classList.add('shadow', 'py-3')
+    } else {
+        navbar.classList.add('py-4')
+        navbar.classList.remove('shadow', 'py-3')
+    }
+})
+
+// AOS
+AOS.init({
+    duration: 700
+});
+
+
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions() {
+    //Button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    //Sctions Active 
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            //resmove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
